80d6786ec47491a485f1c376dffb2ee278abed42
resxustrya <rexustraya@gmail.com>
2017-01-30 15:10:18 +0800

    Push update

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -7,6 +7,7 @@
  */
 
 namespace App\Http\Controllers;
+use App\Leave;
 use Illuminate\Http\Request;
 
 class DocumentController extends Controller
@@ -21,8 +22,33 @@ class DocumentController extends Controller
         if($request->isMethod('get')){
             return view('form.leave');
         }
-        if($request->isMethod('post')){
-            return $request->all();
+        if($request->isMethod('post')) {
+            $leave = new Leave();
+
+            $leave->userid = $request->user()->userid;
+            $leave->office_agency = $request->input('office_agency');
+            $leave->lastname = $request->input('lastname');
+            $leave->firstname = $request->input('firstname');
+            $leave->middlename = $request->input('middlename');
+            $leave->date_filling = $request->input('date_filling');
+            $leave->position = $request->input('position');
+            $leave->month_salary = $request->input('month_salary');
+            $leave->vication_leave_type = $request->input('vication_leave_type');
+            $leave->vacation_others = $request->input('vacation_others');
+            $leave->sick_leave_type = $request->input('sick_leave_type');
+            $leave->sick_others = $request->input('sick_others');
+            $leave->vacation_loc = $request->input('vacation_loc');
+            $leave->abroad_others = $request->input('abroad_others');
+            $leave->med_loc = $request->input('med_loc');
+            $leave->out_patient_others = $request->input('out_patient_others');
+            $leave->applied_num_days = $request->input('applied_num_days');
+            $leave->inc_from = $request->input('inc_from');
+            $leave->inc_to =  $request->input('inc_to');
+            $leave->requested = $request->input('requested');
+
+            $leave->save();
+            return $leave->id;
+
         }
     }
     public function so(Request $request)
---
+++ storage/framework/views/31a829c5e18b958d5be430860a71552796b1ea52.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Test Forms</title>
+</head>
+<body>
+<form action="<?php echo e(asset('test/form')); ?>" method="POST">
+    <?php echo e(csrf_field()); ?>
+
+    <table border="0">
+        <tr>
+            <td><input type="radio" name="color" value="Red" /></td>
+            <td><label for="color">Red</label></td>
+        </tr>
+        <tr>
+            <td><input type="radio" name="color" value="Black" /></td>
+            <td><label for="color">Black</label> </td>
+        </tr>
+        <tr>
+            <td><input type="radio" name="color" value="Orange" /> </td>
+            <td><label for="color">Orange</label> </td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="submit" value="Submit" /> </td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
